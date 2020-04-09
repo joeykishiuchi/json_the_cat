fa00dc7ca933cf6bd45b558aa14b7cd21b4e3ac0
Joey Kishiuchi <your@email.comcd>
2020-04-09 21:32:29 +0000

    Inital commit. Completed program

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,15 @@
 const request = require('request');
-const argv = process.argv.slice(2);
-const URL = `https://api.thecatapi.com/v1/breeds/search?q=${argv}`;
 
-request(URL, (err, resp, body) => {
-  if (err) console.log(err);
-  const data = JSON.parse(body);
-  if (data[0]) {
-    console.log(data[0]['description']);
-  } else {
-    console.log('Sorry, That\'s not a breed of Cat!');
-  }
-});
+const fetchBreedDescription = function(breed, callback) {
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  request(URL, (err, resp, body) => {
+    const data = JSON.parse(body);
+    if (data[0]) {
+      callback(err, data[0]['description']);
+    } else {
+      callback(err, 'Breed Doesn\'t Exits');
+    }
+  });
+};
+
+module.exports = fetchBreedDescription;
